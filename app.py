522b22fa2910fde462ea750c50515cb4d68c3cf1
Ahmad_Just_pub_use_only <abdullahhhmuhhhammad@gmail.com>
2025-04-11 17:07:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask, jsonify
 from datetime import datetime
+import json
 
 app = Flask(__name__)
 
+@app.route('/datetime', methods=['GET'])
+def default_function():
+    return get_current_datetime()
 @app.route('/', methods=['GET'])
 def get_current_datetime():
-    # Get the current date and time
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    # Return a JSON response
-    return jsonify({'datetime': current_time})
+   
+    current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    
+    output = {
+        "status": "OK",
+        "message": "success",
+        "datetime": current_datetime
+    }
+
+    
+    return jsonify(output)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
